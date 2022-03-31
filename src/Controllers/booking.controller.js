05f42dd8f25ca0cef567a74c207ad8a1b4b02f5b
Asliddin Buriev <asliddin00buriev@gmail.com>
2022-04-01 01:20:27 +0900

    fix bug

--- src/Controllers/booking.controller.js
+++ src/Controllers/booking.controller.js
@@ -4,38 +4,17 @@ import catchAsyncError from '../Utils/catchAsyncError.js';
 import CustomError from '../Utils/CustomError.js';
 import Stripe from 'stripe';
 
-export const checkout = catchAsyncError(async (req, res, next) => {
-	const tour = await Tour.findById(req.params.tourId);
-	if (!tour) return next(new CustomError('Tour not found', 404));
-	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-	const session = await stripe.checkout.sessions.create({
-		line_items: [
-			{
-				price_data: {
-					currency: 'usd',
-					unit_amount: tour.price * 100,
-					product_data: {
-						name: `${tour.name}`,
-						description: tour.summary,
-						images: [tour.imageCover],
-					},
-				},
-				quantity: 1,
-			},
-		],
-		customer_email: req.user.email,
-		client_reference_id: req.params.tourId,
-		mode: 'payment',
-		payment_method_types: ['card'],
-		success_url: `${req.protocol}://${req.get('host')}/?tour=${
-			req.params.tourId
-		}&user=${req.user._id}&price=${tour.price}`,
-		cancel_url: `${req.protocol}://${req.get('host')}`,
-	});
-	res.status(200).json({
-		status: 'success',
-		message: 'Successfully created checkout session',
-		url: session.url,
+export const paymentIntent = catchAsyncError(async (req, res, next) => {
+	const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+	const { paymentMethodType, currency, paymentMethodOptions } = req.body;
+	const params = {
+		payment_method_types: [paymentMethodType],
+		amount: 1999,
+		currency: currency,
+	};
+	const paymentIntent = await stripe.paymentIntents.create(params);
+	res.send({
+		clientSecret: paymentIntent.client_secret,
 	});
 });
 
--- src/Routes/booking.routes.js
+++ src/Routes/booking.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { verifyLogin, verifyRole } from '../Controllers/auth.controller.js';
-import { checkout } from '../Controllers/booking.controller.js';
+import { paymentIntent } from '../Controllers/booking.controller.js';
 
 const router = Router();
 
-router.get('/checkout/:tourId', verifyLogin, checkout);
+router.post('/create-payment-intent', verifyLogin, paymentIntent);
 
 export default router;
